237397f145814bb0f5420f73e4461b351b2fbec2
Varun Nirala <nirala.varun@gmail.com>
2023-04-14 17:39:05 +0530

    Changes

--- DesignPattern/src/Singleton.h
+++ DesignPattern/src/Singleton.h
@@ -1,34 +1,47 @@
 #pragma once
 
-#include <iostream>
+#include <mutex>
 
-class Singleton final
+class Singleton
 {
 public:
 	static Singleton& getInstance()
 	{
-		static Singleton *m_pInstance = new Singleton(); 
+		static Singleton m_instance;
+		return m_instance;
+	}
+private:
+	Singleton() = default;
+	~Singleton() = default;
+
+	Singleton(const Singleton&) = delete;
+	Singleton& operator=(const Singleton&) = delete;
+};
+
+class Singleton1
+{
+public:
+	static Singleton1& getInstance()
+	{
+		std::call_once(m_onceFlag, &Singleton1::initSingleton1);
 		return *m_pInstance;
 	}
 
-	void doWork()
-	{
-		std::cout << "I am Singleton\n";
-	}
-
-	Singleton(const Singleton &) = delete;
-	Singleton(Singleton &&) = delete;
-
-	Singleton& operator=(const Singleton &) = delete;
-	Singleton& operator=(Singleton &&) = delete;
-
-	~Singleton()
-	{
-		std::cout << "Dtor : I am Done\n";
-	}
 private:
-	Singleton()
+	Singleton1() = default;
+	~Singleton1() = default;
+
+	Singleton1(const Singleton1&) = delete;
+	Singleton1& operator=(const Singleton1&) = delete;
+
+	static void initSingleton1()
 	{
-		std::cout << "Ctor : I am Alive\n";
+		m_pInstance = new Singleton1();
 	}
+
+	static Singleton1* m_pInstance;
+	static std::once_flag m_onceFlag;
 };
+
+Singleton1* Singleton1::m_pInstance{};
+std::once_flag Singleton1::m_onceFlag;
